9fa5fe657791e583acf783e5c80e8b4df686441b
oiyatsa <s1216ayu@gmail.com>
2024-03-26 10:50:17 +0900

    08-6ブログ削除処理の実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -45,4 +45,9 @@ public function show(Post $post)
 	    return redirect('/posts/' . $post->id);
 	}
 	
+	public function delete(Post $post)
+	{
+	    $post->delete();
+	    return redirect('/');
+	}
 }
